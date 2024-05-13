501f56eaeeedbf9b1016a0a331ac0ecf88fe6810
nikhilpahan <nikhilpahan02@gmail.com>
2024-05-14 00:53:37 +0530

    Storing data in Browser

--- script.js
+++ script.js
@@ -18,6 +18,11 @@ function addTask(){
 }
 
 
-listContainer.addEventListener("click", function(e)) {
-    
-}
+listContainer.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+    }
+}, false);
